db21018d98f0fc7015cb58a60fc02b53cb5276a1
nitishjaiswal2002 <141999440+nitishjaiswal2002@users.noreply.github.com>
2023-09-19 18:25:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
 document.addEventListener("DOMContentLoaded", () => {
-	document.body.innerText ="DOM Load sucess";
-})
+	document.body.innerText ="DOM load success";
+});
